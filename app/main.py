37ae439e30154d0b27e281cc25698adeff810dcd
sarthakbachhar <sarthakbachhar7@gmail.com>
2025-07-07 19:15:50 +0545

    Added frontend HTML template and updated Flask app to render UI

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 # app/main.py
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Secure CI/CD!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host="0.0.0.0", debug=False)
